13b0b30e06c7e10321b268335d3e621500bd9a52
Mona Saad <130683461+Monasaad258@users.noreply.github.com>
2024-01-30 18:30:42 +0200

    Add files via upload

---
+++ javascript gd/main.js
@@ -0,0 +1,49 @@
+// <!-- task1(2) -->
+// console.log("%cElzero","color:blue;font-size:80px; font-size: 80px;font-weight: bold;text-align: center;font-family: Arial");
+
+
+
+
+
+// <!-- task1(6) -->
+// comment 
+/*console.log("Iam In Console");
+document.write("Iam In Page");*/
+
+
+
+
+
+// <!-- task2(1) -->
+// var numberOne =10;
+// var numberTwo =20;
+// console.log(numberOne+""+numberTwo);
+// console.log(typeof( numberOne+""+ numberTwo));
+// console.log(`${numberOne}${numberTwo}`);
+// console.log(typeof(`${numberOne}${numberTwo}`));
+// console.log(numberTwo+"\n"+numberOne);
+// console.log(`${numberTwo}
+// ${numberOne}`);
+
+
+//  <!-- task2(3) -->
+// var a="`I'm In";
+// var b= "Love";
+// var c= "with";
+// var d="javascript";
+// var e="``"
+// console.log(`${a}
+// \\\\
+// ${b}\\\\ """ '''
+// ++ ${c}++
+// \\"""\\"""
+// ""${d}""${e}
+// `);
+
+
+// <!-- task2(4) -->
+// let a = 21;
+// let b = 20;
+// console.log(`_${a}_${b}${a}_${b}${a}_${b}${a}_${b}_`);
+
+
